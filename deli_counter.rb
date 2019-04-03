d56d97d68ff86497a36976fba97bcf9d367085fe
Thomas Laforgia <thomaslaforgia@gmail.com>
2019-04-03 17:37:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,4 @@ katz_deli = []
 def line(deli=katz_deli)
   if deli.length = 0
     puts "The line is currently empty."
-  else
-    deli.each do |item, index|
-      index = index + 1
-      puts "#{index}. #{item}"
-    end
-
 end
-
