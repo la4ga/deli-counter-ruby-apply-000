64883fc18c43e939af54b31b8f75eef18f83f8b1
Thomas Laforgia <thomaslaforgia@gmail.com>
2019-04-03 17:36:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli=katz_deli)
+  if deli.length = 0
+    puts "The line is currently empty."
+  else
+    deli.each do |item, index|
+      index = index + 1
+      puts "#{index}. #{item}"
+    end
+
+end
+
+def take_a_number(deli, name)
+  # TODO: write
+end
+
+def now_serving(deli=katz_deli)
+  ## TODO: write
+end
