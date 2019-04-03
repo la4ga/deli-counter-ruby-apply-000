88b2625cda947d24edaae07d8e43939933868fe9
Thomas Laforgia <thomaslaforgia@gmail.com>
2019-04-03 18:11:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def line(deli=katz_deli)
 end
 
 def take_a_number(deli, name)
-  deli = deli << name
+  deli << name
   position_in_line = deli.length
   puts "Welcome, #{name}. You are number #{position_in_line} in line."
 end
