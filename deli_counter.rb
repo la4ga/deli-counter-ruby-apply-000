81c94d6500facd629003c1db436d847c8758c25a
Thomas Laforgia <thomaslaforgia@gmail.com>
2019-04-03 18:14:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(deli=katz_deli)
   else
     line_string = 'The line is currently:'
     deli.each_with_index do |item, index|
-      line_string << " #{index}. #{item}"
+      line_string << " #{index += 1}. #{item}"
     end
     puts line_string
   end
